fd7a5be3492cbf2ad044e1399111b6259aec6a99
James Ganza <sanganzales@gmail.com>
2022-06-09 11:06:00 +0200

    added statsOverView code

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Order;
+use App\Models\Payment;
+use App\Models\PurchaseOrder;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 
@@ -9,10 +13,41 @@ class StatsOverview extends BaseWidget
 {
     protected function getCards(): array
     {
+
+        $user = User::find(auth()->id())->load('userHasCounter');
+
+        if($user->userHasCounter)
+        {
+            $totalWeeklyOrders =Order::where('counterId',$user->UserHasCounter->counterId)
+                                    ->whereBetween('created_at',[now()->startOfWeek(),now()->endOfWeek()])
+                                    ->count();
+            $totalWeeklyIncomes = Payment::join('orders','orders.id','payments.orderId')
+                                    ->where(['transactionId'=>1])
+                                    ->where('counterId',$user->UserHasCounter->counterId)
+                                    ->whereBetween('payments.created_at',[now()->startOfWeek(),now()->endOfWeek()])
+                                    ->sum('payments.amount');
+        }
+        else
+        {
+            $totalWeeklyOrders = Order::where('counterId','!=',0)
+                    ->whereBetween('created_at',[now()->startOfWeek(),now()->endOfWeek()])
+                    ->count();
+            $totalWeeklyIncomes = Payment::join('orders','orders.id','payments.orderId')
+                    ->where(['transactionId'=>1])
+                    ->where('counterId','!=',0)
+                    ->whereBetween('payments.created_at',[now()->startOfWeek(),now()->endOfWeek()])
+                    ->sum('payments.amount');
+        }
+
+        $totalWeeklyPuchases = Payment::join('purchase_orders','purchase_orders.id','payments.orderId')
+                                    ->where(['transactionId'=>2])
+                                    ->whereBetween('payments.created_at',[now()->startOfWeek(),now()->endOfWeek()])
+                                    ->sum('payments.amount');
+
         return [
-            Card::make('Unique views', '192.1k'),
-            Card::make('Bounce rate', '21%'),
-            Card::make('Average time on page', '3:12'),
+            Card::make('Weekly Orders', $totalWeeklyOrders),
+            Card::make('Weekly Incomes', $totalWeeklyIncomes),
+            Card::make('Weekly Expenses', $totalWeeklyPuchases),
         ];
     }
 }
