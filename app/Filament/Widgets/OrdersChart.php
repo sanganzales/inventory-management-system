a93cf71d4f7bc1100c501d02b811419fd9e1623f
James Ganza <sanganzales@gmail.com>
2022-06-09 11:41:24 +0200

    added OrdersChart with code

--- app/Filament/Widgets/OrdersChart.php
+++ app/Filament/Widgets/OrdersChart.php
@@ -2,25 +2,37 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Order;
 use Filament\Widgets\LineChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 
 class OrdersChart extends LineChartWidget
 {
     protected function getHeading(): string
     {
-        return 'Blog posts';
+        return 'Orders';
     }
 
     protected function getData(): array
     {
+        $data = Trend::model(Order::class)
+        ->between(
+            start: now()->startOfYear(),
+            end: now()->endOfYear(),
+        )
+        ->perMonth()
+        ->count();
+
+
         return [
             'datasets' => [
                 [
                     'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
         ];
     }
 }
