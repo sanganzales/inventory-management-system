d538621a180150db200ab185d1e0154c6c5f31e4
James Ganza <sanganzales@gmail.com>
2022-06-09 10:46:47 +0200

    added statsOverView example

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -10,7 +10,9 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            //
+            Card::make('Unique views', '192.1k'),
+            Card::make('Bounce rate', '21%'),
+            Card::make('Average time on page', '3:12'),
         ];
     }
 }
