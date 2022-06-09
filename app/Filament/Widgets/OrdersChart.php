fc06fa4c3dbaeec803957ac3050f39909616f083
James Ganza <sanganzales@gmail.com>
2022-06-09 09:21:54 +0200

    commenting Orders on dashboard

--- app/Filament/Widgets/OrdersChart.php
+++ app/Filament/Widgets/OrdersChart.php
@@ -8,32 +8,32 @@ use Illuminate\Support\Facades\DB;
 
 class OrdersChart extends LineChartWidget
 {
-    protected static ?string $heading = 'Orders per month';
+    // protected static ?string $heading = 'Orders per month';
 
-    protected static ?int $sort = 1;
+    // protected static ?int $sort = 1;
 
-    protected function getData(): array
-    {
-        $orders = Order::select(DB::raw("(count(id)) as total_orders"),
-                                DB::raw("(DATE_FORMAT(created_at, '%M')) as month"),
-                                DB::raw("(DATE_FORMAT(created_at, '%m-%Y')) as month_year"))
-                        ->orderBy('month_year')
-                        ->groupBy(['month','month_year'])
-                        ->get();
-        foreach($orders as $order)
-        {
-            $data[] = $order->total_orders;
-            $labels[]=$order->month;
-        }
-        //dd($orders);
-        return [
-            'datasets' => [
-                [
-                    'label' => 'Orders',
-                    'data' => $data,
-                ],
-            ],
-            'labels' => $labels,
-        ];
-    }
+    // protected function getData(): array
+    // {
+    //     $orders = Order::select(DB::raw("(count(id)) as total_orders"),
+    //                             DB::raw("(DATE_FORMAT(created_at, '%M')) as month"),
+    //                             DB::raw("(DATE_FORMAT(created_at, '%m-%Y')) as month_year"))
+    //                     ->orderBy('month_year')
+    //                     ->groupBy(['month','month_year'])
+    //                     ->get();
+    //     foreach($orders as $order)
+    //     {
+    //         $data[] = $order->total_orders;
+    //         $labels[]=$order->month;
+    //     }
+    //     //dd($orders);
+    //     return [
+    //         'datasets' => [
+    //             [
+    //                 'label' => 'Orders',
+    //                 'data' => $data,
+    //             ],
+    //         ],
+    //         'labels' => $labels,
+    //     ];
+    // }
 }
